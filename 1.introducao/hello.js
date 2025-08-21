00f101e3724ce2e63e9cbe7cdd37f2f55d7291b9
Miguel Antonio Gurgel <2025114000044@pcs.ifsuldeminas.local>
2025-08-21 16:10:43 -0300

    adicionando codigos ao repositorio

---
+++ 1.introducao/hello.js
@@ -0,0 +1,3 @@
+console.log("\n hello word");
+
+console.log("----------");
---
+++ 1.introducao/operadores.js
@@ -0,0 +1,8 @@
+let x=1, y=2;
+
+console.log("soma= "+(x+y));
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log("----------");
+console.log("1"+2);
